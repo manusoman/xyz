affdf160bd2672e4e0fe17ff46ff6e5e844063ba
Manu Soman <manu.naturep@gmail.com>
2020-06-26 14:44:31 +0530

    math animation added

---
+++ Math_Anim.js
@@ -0,0 +1,175 @@
+window.mathtrix = {};
+
+mathtrix.config = {
+    unit : 50,
+    dx : 1
+};
+
+(function() { 'use strict';
+
+    let Complex = function(a) {
+        this.r = a[0] || 0;
+        this.i = a[1] || 0;
+    }
+
+    Complex.prototype = {
+        constructor : Complex,
+
+        add : function(v) {
+            if(typeof(v) === 'number') return new Complex([this.r + v, this.i]);
+            return new Complex([this.r + v.r, this.i + v.i]);
+        },
+
+        multiply : function(v) {
+            if(typeof(v) === 'number') return new Complex([this.r * v, this.i * v]);
+            return new Complex([(this.r * v.r) - (this.i * v.i), (this.i * v.r) + (this.r * v.i)]);
+        }
+    };
+
+    Complex.exp = function(rad) {
+        return new Complex([Math.cos(rad), Math.sin(rad)]);
+    };
+
+
+    mathtrix.Complex = Complex;
+
+})();
+
+(function() { 'use strict';
+
+    const { unit } = mathtrix.config,
+        canvas_ele = document.getElementById('canvas'),
+        context = canvas_ele.getContext('2d');
+
+    let width = canvas_ele.width = window.innerWidth,
+        height = canvas_ele.height = window.innerHeight,
+        w = width / 2,
+        h = height / 2;
+
+
+    const canvas = {
+        get context() { return context; },
+
+        get xMin() { return -w; },
+        get xMax() { return w; },
+        get yMin() { return -h; },
+        get yMax() { return h; },
+
+        scale : function(x, y) {
+            context.scale(x, y);
+        },
+
+        drawCordinatePlane : function(subDiv = false) {
+            let aw = 2,
+                tw = 0.3,
+                ts = 5,
+                xMin = -w,
+                xMax = w,
+                yMin = -h,
+                yMax = h;
+
+            context.strokeStyle = '#a0a0a0';
+            context.lineWidth = aw;
+            context.beginPath();
+            context.moveTo(xMin, 0);
+            context.lineTo(xMax, 0);
+            context.moveTo(0, yMin);
+            context.lineTo(0, yMax);
+            context.stroke();
+
+            context.lineWidth = tw;
+
+            if(subDiv == 'tick') {
+                for(let i = unit; i < xMax; i += unit) {
+                    context.moveTo(i, ts);
+                    context.lineTo(i, -ts);
+                    context.moveTo(-i, ts);
+                    context.lineTo(-i, -ts);
+                }
+                for(let i = unit; i < yMax; i += unit) {
+                    context.moveTo(-ts, i);
+                    context.lineTo(ts, i);
+                    context.moveTo(-ts, -i);
+                    context.lineTo(ts, -i);
+                }
+                context.stroke();
+
+            } else if(subDiv == 'grid') {
+                for(let i = unit; i < xMax; i += unit) {
+                    context.moveTo(i, yMax);
+                    context.lineTo(i, yMin);
+                    context.moveTo(-i, yMax);
+                    context.lineTo(-i, yMin);
+                }
+                for(let i = unit; i < yMax; i += unit) {
+                    context.moveTo(xMin, i);
+                    context.lineTo(xMax, i);
+                    context.moveTo(xMin, -i);
+                    context.lineTo(xMax, -i);
+                }
+                context.stroke();
+            }
+        },
+
+
+        setCordinatePlane : function(type) {
+            switch(type) {
+                case 'cartitian':
+                    context.translate(w, h);
+                    context.scale(1, -1);
+                    break;
+
+                case 'cartitian-Q1':
+                    context.translate(100, height - 100);
+                    context.scale(1, -1);
+                    break;
+                
+                default:
+            }
+        },
+
+
+        clearScreen : function() {
+            context.clearRect(-w, -h, width, height);
+        }
+    };
+
+    mathtrix.canvas = canvas;
+
+})();
+
+(function() { 'use strict';
+
+    const { unit, dx } = mathtrix.config,
+        { context, xMin, xMax } = mathtrix.canvas;
+
+
+    const drawFuncs = {
+
+        plotSinWave : function({freq = 1, phShift = 0, amp = 1, min = xMin, max = xMax}) {
+            let _freq = Math.PI * 2 * freq,
+                _phShift = Math.PI * 2 * phShift,
+                UxA = unit * amp;
+
+            context.strokeStyle = '#000000';
+            context.lineWidth = 2;
+            context.beginPath();
+            context.moveTo(min, Math.sin((_freq * min / unit) + _phShift) * UxA);
+
+            for(let x = min + dx; x <= max; x += dx) {
+                context.lineTo(x, Math.sin((_freq * x / unit) + _phShift) * UxA);
+            }
+
+            context.stroke();
+        },
+
+        getBaseLog : function(base, value) {
+            return Math.log(value) / Math.log(base);
+        }
+    };
+
+
+
+    mathtrix.drawFuncs = drawFuncs;
+
+})();
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function() { 'use strict';
 
-    const blinkSpeed = 500,
+    const blinkSpeed = 1000,
         elements = {
             body : document.body,
             texts : [document.getElementById('yaa'), document.getElementById('hoo'), document.getElementById('woo'), document.getElementById('haa')],
@@ -36,5 +36,43 @@
 
         return color;
     }
+
+})();
+
+
+
+(function() { 'use strict';
+
+    const { unit } = mathtrix.config,
+        { Complex } = mathtrix,
+        { context, setCordinatePlane, clearScreen } = mathtrix.canvas,
+        TwoPI = 2 * Math.PI;
+
+        setCordinatePlane('cartitian');
+
+        let wind = 0;
+
+        function fourier() {
+            clearScreen();
+
+            context.strokeStyle = '#ffffff';
+            context.lineWidth = 1;
+            context.beginPath();
+            context.moveTo(5 * unit, 0);
+
+            for(let c = 0; c < 10 * unit; c += 0.2) {
+                let _c = c / unit,
+                    sc = Math.sin(TwoPI * _c) * 2,
+                    p = Complex.exp(TwoPI * wind * _c).multiply(sc + 5);
+
+                context.lineTo(p.r * unit, p.i * unit);
+            }
+
+            context.stroke();
+            wind += 0.001;
+        }
+
+
+        setInterval(fourier, 40);
 
 })();
