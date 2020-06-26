968486f3d7bdf936f607bc333f2d8a8741248521
Manu Soman <manu.naturep@gmail.com>
2020-06-26 19:57:01 +0530

    modified

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 (function() { 'use strict';
 
-    const blinkSpeed = 1000,
+    const { unit } = mathtrix.config,
+        { Complex } = mathtrix,
+        { context, setCordinatePlane, clearScreen } = mathtrix.canvas,
+        TwoPI = 2 * Math.PI,
+        blinkSpeed = 1000,
         elements = {
             body : document.body,
             texts : [document.getElementById('yaa'), document.getElementById('hoo'), document.getElementById('woo'), document.getElementById('haa')],
@@ -8,13 +12,23 @@
         };
 
     let txtLen = elements.texts.length,
-        counter = 0;
+        counter = 0,
+        wind = 0,
+        speed = 0.001;
+
+        setCordinatePlane('cartitian');
+
+        context.strokeStyle = '#ffffff';
+        context.lineWidth = 1;
 
 
     setInterval(function() {
         switchTextNColor();
         elements.body.style.backgroundColor = getRandomColor();
+        context.strokeStyle = getRandomColor();
     }, blinkSpeed);
+
+    setInterval(fourier, 40);
 
 
     function switchTextNColor() {
@@ -37,45 +51,23 @@
         return color;
     }
 
-})();
+    function fourier() {
+        clearScreen();
+        context.beginPath();
+        context.moveTo(3 * unit, 0);
 
+        for(let c = 0; c < 10 * unit; c += 0.2) {
+            let _c = c / unit,
+                sc = -Math.cos(TwoPI * _c) * 2,
+                p = Complex.exp(TwoPI * wind * _c).multiply(sc + 5);
 
-
-(function() { 'use strict';
-
-    const { unit } = mathtrix.config,
-        { Complex } = mathtrix,
-        { context, setCordinatePlane, clearScreen } = mathtrix.canvas,
-        TwoPI = 2 * Math.PI;
-
-        setCordinatePlane('cartitian');
-
-        let wind = 0,
-            speed = 0.001;
-
-        function fourier() {
-            clearScreen();
-
-            context.strokeStyle = '#ffffff';
-            context.lineWidth = 1;
-            context.beginPath();
-            context.moveTo(3 * unit, 0);
-
-            for(let c = 0; c < 10 * unit; c += 0.2) {
-                let _c = c / unit,
-                    sc = -Math.cos(TwoPI * _c) * 2,
-                    p = Complex.exp(TwoPI * wind * _c).multiply(sc + 5);
-
-                context.lineTo(p.r * unit, p.i * unit);
-            }
-
-            context.stroke();
-            wind += speed;
-
-            if(wind > 5 || wind < -5) speed = -speed;
+            context.lineTo(p.r * unit, p.i * unit);
         }
 
+        context.stroke();
+        wind += speed;
 
-        setInterval(fourier, 40);
+        if(wind > 5 || wind < -5) speed = -speed;
+    }
 
 })();
